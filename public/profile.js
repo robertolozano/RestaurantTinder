08af636112ade92ed2f3bbdb92fd1f452a36ba06
Roberto Lozano <rlozano@ucdavis.edu>
2021-03-27 23:33:03 -0700

    Remove restaurants.db file, no longer needed since the switch from SQLite3 to firebase

--- public/profile.js
+++ public/profile.js
@@ -41,53 +41,50 @@ function setUp2(imageurl, index, name) {
     card_info_div.appendChild(restaurant_address);
 }
 
-
-
 function signIn(){
     console.log("pressed sign in button")
 
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-        // User is signed in.
-        console.log("user " + user.displayName + " is already signed in");
-        window.location.href = "profile.html"
+            // User is signed in.
+            console.log("user " + user.displayName + " is already signed in");
+            window.location.href = "profile.html"
 
         } else {
-        firebase.auth().signInWithPopup(provider).then((result) => {
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            console.log(user)
+            firebase.auth().signInWithPopup(provider).then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                console.log(user)
 
-            database.ref('/users/').update({
-                userID: user.email
+                database.ref('/users/').update({
+                    userID: user.email
+                });
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
             });
-        }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-        });
         }
     });
 }
   
-
 function setUp(){
     let signInButton = document.getElementById("sign_in_button");
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-        console.log("user already signed in should be changed to profile")
-        signInButton.textContent = "Profile"
+            console.log("user already signed in should be changed to profile")
+            signInButton.textContent = "Profile"
         } else {
-        console.log('user not signed in change to log in')
-        signInButton.textContent = "Log In"
+            console.log('user not signed in change to log in')
+            signInButton.textContent = "Log In"
         }
     });
 }
